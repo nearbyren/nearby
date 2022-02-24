d61b290e72ea70d225dd56df5bfdfdc87c27c070
wangcunrong@ejiayou.com <wangcunrong@ejiayou.com>
2022-02-24 17:15:37 +0800

    ...

---
+++ app/src/main/java/ren/nearby/SplashActivity.kt
@@ -0,0 +1,16 @@
+package ren.nearby
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author:
+ * @created on: 2022/2/24 14:31
+ * @description:
+ */
+class SplashActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
